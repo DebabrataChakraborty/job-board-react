1ac2fac8dc6fcdaf18cd2b37049ddc1e3c55e901
subham141198 <banerjee.subham1411@gmail.com>
2023-04-11 21:54:31 +0530

    design part left

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -1,23 +1,51 @@
 import { useParams, useLoaderData } from "react-router-dom";
 import Header from "../../components/Header/Header";
-import { useEffect } from "react";
+import { useEffect , useState } from "react";
 
 export default function Details() {
+let jobdata = []
   const { detail_id } = useParams();
   const datas = useLoaderData();
-    console.log(datas);
-    console.log(detail_id);
-
-//   useEffect(() => {
-//     if(datas){
-//         // const singleFeature =  datas.jobs.find(feature => feature.data.id === detail_id);
-//     }
-//   },[])
+  for(let job  in datas.jobs){
+      if(datas.jobs[job].id == detail_id){
+          jobdata = datas.jobs[job] 
+      }
+  }
+  console.log(jobdata)
   
+
   return (
     <>
       <Header />
-      {/* Render job details here */}
+
+      <div class="container">
+        <div className="row">
+          <div className="col-12">
+            <h1 className="text-center">Job details</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-7 descrip-wrapper">
+            <div className="row">
+              <div className="col-12">
+                 <p>{jobdata.job_description}</p>
+              </div>
+              <div className="col-12">
+                {jobdata.job_responsibilities.map(jobRespon =>{
+                  console.log(jobRespon)
+                }
+                  )}
+                 <p></p>
+              </div>
+            </div>
+              
+
+          </div>
+          <div className="col-5 details-wrapper">
+            
+          </div>
+        </div>
+      </div>
     </>
   );
 }
